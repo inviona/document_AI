db68b4cd4390748b58fe0bbcac306b47f76fd97f
inviona <inviajna@gmail.com>
2023-10-03 00:18:26 +0200

    I am working on a new project which is about an AI project based. You will be able to upload an document on it and after that you can ask the computer anything about the pdf file and the computer will answer the questions

---
+++ my-next-app/src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from 'next/image'
+import MaxWidthWrapper from './components/MaxWidthWrapper'
+
+export default function Home() {
+  return (
+    <MaxWidthWrapper className='bg-red-500' ><div></div></MaxWidthWrapper>
+  )
+}
